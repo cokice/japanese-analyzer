6cf549a442e0af10ca71baaa94e815803c15572d
Howen_Huang <huang.h.11@pg.com>
2026-06-17 02:38:29 +0900

    Refresh dark theme colors and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#f7f6fb' },
-    { media: '(prefers-color-scheme: dark)', color: '#16131f' }
+    { media: '(prefers-color-scheme: dark)', color: '#272238' }
   ],
 };
 
